098bc77aae250516ed7e72d8b631e561bb4266e9
Facuindo <facu_nicogas@outlook.com>
2023-12-15 18:33:56 -0300

    Añadir templates, formulario

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ from flask_sqlalchemy import SQLAlchemy
 #Crear el server Flask
 app = Flask(__name__)
 
+#Renderiza Index.html 
+@app.route("/")
+def index():
+    try:
+        return render_template('index.html')
+    except:
+        return jsonify({'trace': traceback.format_exc()})
+    
+    
 if __name__ == '__main__':
-
-    #lanzar server
-    app.run(host="127.0.0.1", port=5000)
+    #Lanzar server
+    app.run(host="127.0.0.1", port=5000, debug= True)
